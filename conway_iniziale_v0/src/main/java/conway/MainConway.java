1101cdcf1149af38d7a6cb386c551cb13dd833cc
Lorenzo-Zoccadelli <lorenzo.zoccadelli@studio.unibo.it>
2025-02-26 14:47:19 +0100

    Feb26

---
+++ conway_iniziale_v0/src/main/java/conway/MainConway.java
@@ -0,0 +1,21 @@
+package conway;
+
+import conway.device.ConwayInputMock;
+
+public class MainConway  {
+
+    public static void main(String[] args) {
+
+      //configureTheSystem
+        Life life           =
+                new Life( 3,3 );
+        LifeController cc   =
+                new LifeController(life);
+        ConwayInputMock cim =
+                new ConwayInputMock(cc,life);
+
+      //start the system
+        cim.simulateUserControl();
+
+    }
+}
---
+++ conway_iniziale_v0/src/main/java/conway/device/ConwayOutput.java
@@ -0,0 +1,13 @@
+package conway.device;
+
+import conway.IOutDev;
+
+//
+public class ConwayOutput implements IOutDev{
+ 
+	
+	public void displayCell(String msg) {
+		System.out.print(msg);
+	}
+	
+}
