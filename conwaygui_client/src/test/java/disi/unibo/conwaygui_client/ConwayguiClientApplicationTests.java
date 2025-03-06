1ced35234d1f24dac405c9a072d67c176a5f255d
Lorenzo-Zoccadelli <lorenzo.zoccadelli@studio.unibo.it>
2025-03-06 17:03:48 +0100

    Mar06

---
+++ conwaygui_client/src/test/java/disi/unibo/conwaygui_client/ConwayguiClientApplicationTests.java
@@ -0,0 +1,13 @@
+package disi.unibo.conwaygui_client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConwayguiClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- conwaygui_v1_spring/src/main/java/unibo/disi/conwaygui/conway/Life.java
+++ conwaygui_v1_spring/src/main/java/unibo/disi/conwaygui/conway/Life.java
@@ -37,7 +37,7 @@ public class Life {
         //CommUtils.outyellow("Life | initializeGrids done");
     }
 
-    protected void resetGrids() {
+    public void resetGrids() {
          createGrids();
     }
 
