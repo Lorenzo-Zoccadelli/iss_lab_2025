91b6ca37ab167b82a2f3e054ffd7c2f37e5e8502
Lorenzo-Zoccadelli <lorenzo.zoccadelli@studio.unibo.it>
2025-04-23 10:58:07 +0200

    .

---
+++ con_raspi/sonar_qak/sonar_qak.pl
@@ -0,0 +1,13 @@
+%====================================================================================
+% sonar_qak description   
+%====================================================================================
+mqttBroker("192.168.1.193", "1883", "sonar_qak/events").
+dispatch( sonarstart, sonarstart(X) ).
+dispatch( sonarstop, sonarstop(X) ).
+event( sonardata, distance(D) ).
+%====================================================================================
+context(ctx_sonar, "localhost",  "TCP", "8080").
+ qactor( datacleaner, ctx_sonar, "it.unibo.datacleaner.Datacleaner").
+ static(datacleaner).
+  qactor( sonar, ctx_sonar, "it.unibo.sonar.Sonar").
+ static(sonar).
